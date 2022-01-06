384c3aa6d1b381a8a0460819ede6abecc6724c6b
mateo <teodc4447@gmail.com>
2022-01-06 14:04:51 -0300

    cambios

--- src/controlers/getCountries.js
+++ src/controlers/getCountries.js
@@ -5,36 +5,36 @@ const { Country } = require("../db.js");
 
 router.get("/", async (req, res, next) => {
   try {
-    const country = (await axios(`https://restcountries.com/v3/all`)).data;
-      country?.forEach( (e) => {
-        const { cca2, name, flags, region, area, population } = e;
-        try{
-          Country?.findOrCreate({
-           where: {
-             id: cca2,
-             name: name.common,
-             imagen: flags[1],
-             continente: region,
-             subregion: e?.subregion ? e.subregion : "no tiene subregion",
-             poblacion: parseInt(population),
-             area: parseFloat(area),
-             capital: e?.capital ? e.capital[0] : "No tiene capital",
-           },
-         });
+    // const country = (await axios(`https://restcountries.com/v3/all`)).data;
+    //   country?.forEach( (e) => {
+    //     const { cca2, name, flags, region, area, population } = e;
+    //     try{
+    //       Country?.findOrCreate({
+    //        where: {
+    //          id: cca2,
+    //          name: name.common,
+    //          imagen: flags[1],
+    //          continente: region,
+    //          subregion: e?.subregion ? e.subregion : "no tiene subregion",
+    //          poblacion: parseInt(population),
+    //          area: parseFloat(area),
+    //          capital: e?.capital ? e.capital[0] : "No tiene capital",
+    //        },
+    //      });
    
-          Country.findAll().then((dbCountries) => {
-           res.status(200).json(dbCountries);
-         });
+    //       Country.findAll().then((dbCountries) => {
+    //        res.status(200).json(dbCountries);
+    //      });
           
-        }catch(err){
-          res.send(err)
-        }
-      });
+    //     }catch(err){
+    //       res.send(err)
+    //     }
+    //   });
 
-      // await Country.findAll({
-      // }).then((dbCountries) => {
-      //   res.status(200).json(dbCountries);
-      // });
+      await Country.findAll({
+      }).then((dbCountries) => {
+        res.status(200).json(dbCountries);
+      });
   } catch (err) {
     next(err);
   }
